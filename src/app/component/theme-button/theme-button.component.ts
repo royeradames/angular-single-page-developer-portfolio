9edb5130389de74f56c74a731c0d6967ffe2dd6e
Royer Adames <royeraadames@gmail.com>
2023-05-04 13:45:05 -0400

    theme button

--- src/app/component/theme-button/theme-button.component.ts
+++ src/app/component/theme-button/theme-button.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-theme-button',
+  selector: 'theme-button',
   templateUrl: './theme-button.component.html',
-  styleUrls: ['./theme-button.component.scss']
+  styleUrls: ['./theme-button.component.scss'],
 })
-export class ThemeButtonComponent {
-
-}
+export class ThemeButtonComponent {}
