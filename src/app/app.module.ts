e016deb0c30c207040025ab36bd3bc3e8c57c690
Royer Adames <royeraadames@gmail.com>
2023-05-04 13:21:33 -0400

    init theme button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ThemeButtonComponent } from './component/theme-button/theme-button.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { CommonModule } from '@angular/common';
     HeaderComponent,
     MainComponent,
     FooterComponent,
+    ThemeButtonComponent,
   ],
   imports: [
     ReactiveFormsModule,
