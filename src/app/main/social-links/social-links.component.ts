5920f259dd689492644e2e0ad2f84ca307378537
Royer Adames <royeraadames@gmail.com>
2023-05-04 13:16:25 -0400

    add icons programmatically

--- src/app/main/social-links/social-links.component.ts
+++ src/app/main/social-links/social-links.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-social-links',
   templateUrl: './social-links.component.html',
-  styleUrls: ['./social-links.component.scss']
+  styleUrls: ['./social-links.component.scss'],
 })
 export class SocialLinksComponent {
-
+  icons = [
+    {
+      src: 'assets/svg/icons-github-fill.svg',
+      alt: 'github',
+      href: 'https://github.com/royeradames',
+    },
+    {
+      src: 'assets/svg/simple-icons_frontendmentor.svg',
+      alt: 'frontendmentor',
+      href: 'https://github.com/royeradames',
+    },
+    {
+      src: 'assets/svg/LinkedIn.svg',
+      alt: 'LinkedIn',
+      href: 'https://github.com/royeradames',
+    },
+    {
+      src: 'assets/svg/twitter-icons.svg',
+      alt: 'twitter',
+      href: 'https://github.com/royeradames',
+    },
+  ];
 }
