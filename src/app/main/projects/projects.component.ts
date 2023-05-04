409b44545f6dbc49a33c031af8d64a326e89be74
Royer Adames <royeraadames@gmail.com>
2023-05-04 02:11:43 -0400

    projects html

--- src/app/main/projects/projects.component.ts
+++ src/app/main/projects/projects.component.ts
@@ -3,8 +3,51 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      imgSmall: 'assets/images/thumbnail-project-1-small.webp',
+      imgLarge: 'assets/images/thumbnail-project-1-large.webp',
+      alt: 'Project 1',
+      title: 'DESIGN PORTFOLIO',
+      skills: ['HTML', 'CSS'],
+    },
+    {
+      imgSmall: 'assets/images/thumbnail-project-2-small.webp',
+      imgLarge: 'assets/images/thumbnail-project-2-large.webp',
+      alt: 'Project 1',
+      title: 'E-LEARNING LANDING PAGE',
+      skills: ['HTML', 'CSS'],
+    },
+    {
+      imgSmall: 'assets/images/thumbnail-project-3-small.webp',
+      imgLarge: 'assets/images/thumbnail-project-3-large.webp',
+      alt: 'Project 1',
+      title: 'TODO WEB APP',
+      skills: ['HTML', 'CSS', 'JAVASCRIPT'],
+    },
+    {
+      imgSmall: 'assets/images/thumbnail-project-4-small.webp',
+      imgLarge: 'assets/images/thumbnail-project-4-large.webp',
+      alt: 'Project 1',
+      title: 'ENTERTAINMENT WEB APP',
+      skills: ['HTML', 'CSS', 'JAVASCRIPT'],
+    },
+    {
+      imgSmall: 'assets/images/thumbnail-project-5-small.webp',
+      imgLarge: 'assets/images/thumbnail-project-5-large.webp',
+      alt: 'Project 1',
+      title: 'MEMORY GAME',
+      skills: ['HTML', 'CSS', 'JAVASCRIPT'],
+    },
+    {
+      imgSmall: 'assets/images/thumbnail-project-6-small.webp',
+      imgLarge: 'assets/images/thumbnail-project-6-large.webp',
+      alt: 'Project 1',
+      title: 'ART GALLERY SHOWCASE',
+      skills: ['HTML', 'CSS', 'JAVASCRIPT'],
+    },
+  ];
 }
