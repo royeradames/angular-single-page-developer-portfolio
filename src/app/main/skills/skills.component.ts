71135e868386f0096c12d9de68067185ec7f2783
Royer Adames <royeraadames@gmail.com>
2023-05-04 01:46:13 -0400

    about html

--- src/app/main/skills/skills.component.ts
+++ src/app/main/skills/skills.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent {
-
+  skills = [
+    {
+      name: 'HTML',
+      text: '4 Years Experience'
+    },
+    {
+      name: 'CSS',
+      text: '4 Years Experience'
+    }, {
+      name: 'Javascript',
+      text: '4 Years Experience'
+    }, {
+      name: 'Accessibility',
+      text: '4 Years Experience'
+    }, {
+      name: 'React',
+      text: '3 Years Experience'
+    },{
+      name: 'Sass',
+      text: '3 Years Experience'
+    },
+  ]
 }
