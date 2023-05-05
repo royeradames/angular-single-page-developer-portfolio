40794735faaf6dc70da63a26c021d03f16f3fa8b
Royer Adames <royeraadames@gmail.com>
2023-05-05 10:41:44 -0400

    standardize the incoming input | ts errors 🐛

--- src/app/main/projects/projects.component.ts
+++ src/app/main/projects/projects.component.ts
@@ -1,53 +1,170 @@
 import { Component } from '@angular/core';
 
+export interface ProjectsInterface {
+  title: string;
+  skills: string[];
+  url: { code: string; project: string };
+  picture: {
+      img: { src: string; alt: string };
+      sources: { src: string; media: string }[];
+    };
+}
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent {
-  projects = [
+export class ProjectsComponent implements  {
+
+  projects: ProjectsInterface[] = [
     {
-      imgSmall: 'assets/images/thumbnail-project-1-small.webp',
-      imgLarge: 'assets/images/thumbnail-project-1-large.webp',
-      alt: 'Project 1',
       title: 'DESIGN PORTFOLIO',
       skills: ['HTML', 'CSS'],
+      url: {
+        project: '#',
+        code: 'github.com/royeradames',
+      },
+      picture: {
+        sources: [
+          {
+            src: 'assets/images/thumbnail-project-1-small.webp',
+            media: '(max-width: 768px)',
+          },
+          {
+            src: 'assets/images/thumbnail-project-1-large.webp',
+            media: '(max-width: 1024px)',
+          },
+        ],
+        img: {
+          src: 'assets/images/thumbnail-project-1-small.webp',
+          alt: 'Project 1',
+        },
+      },
     },
     {
-      imgSmall: 'assets/images/thumbnail-project-2-small.webp',
-      imgLarge: 'assets/images/thumbnail-project-2-large.webp',
-      alt: 'Project 1',
       title: 'E-LEARNING LANDING PAGE',
       skills: ['HTML', 'CSS'],
+      url: {
+        project: '#',
+        code: 'github.com/royeradames',
+      },
+      picture: {
+        sources: [
+          {
+            src: 'assets/images/thumbnail-project-2-small.webp',
+            media: '(max-width: 768px)',
+          },
+          {
+            src: 'assets/images/thumbnail-project-2-large.webp',
+            media: '(max-width: 1024px)',
+          },
+        ],
+        img: {
+          src: 'assets/images/thumbnail-project-2-small.webp',
+          alt: 'Project 1',
+        },
+      },
     },
     {
-      imgSmall: 'assets/images/thumbnail-project-3-small.webp',
-      imgLarge: 'assets/images/thumbnail-project-3-large.webp',
-      alt: 'Project 1',
       title: 'TODO WEB APP',
       skills: ['HTML', 'CSS', 'JAVASCRIPT'],
+      url: {
+        project: '#',
+        code: 'github.com/royeradames',
+      },
+      picture: {
+        sources: [
+          {
+            src: 'assets/images/thumbnail-project-3-small.webp',
+            media: '(max-width: 768px)',
+          },
+          {
+            src: 'assets/images/thumbnail-project-3-large.webp',
+            media: '(max-width: 1024px)',
+          },
+        ],
+        img: {
+          src: 'assets/images/thumbnail-project-3-small.webp',
+          alt: 'Project 1',
+        },
+      },
     },
     {
-      imgSmall: 'assets/images/thumbnail-project-4-small.webp',
-      imgLarge: 'assets/images/thumbnail-project-4-large.webp',
-      alt: 'Project 1',
       title: 'ENTERTAINMENT WEB APP',
       skills: ['HTML', 'CSS', 'JAVASCRIPT'],
+
+      url: {
+        project: '#',
+        code: 'github.com/royeradames',
+      },
+      picture: {
+        sources: [
+          {
+            src: 'assets/images/thumbnail-project-4-small.webp',
+            media: '(max-width: 768px)',
+          },
+          {
+            src: 'assets/images/thumbnail-project-4-large.webp',
+            media: '(max-width: 1024px)',
+          },
+        ],
+        img: {
+          src: 'assets/images/thumbnail-project-4-small.webp',
+          alt: 'Project 1',
+        },
+      },
     },
     {
-      imgSmall: 'assets/images/thumbnail-project-5-small.webp',
-      imgLarge: 'assets/images/thumbnail-project-5-large.webp',
-      alt: 'Project 1',
       title: 'MEMORY GAME',
       skills: ['HTML', 'CSS', 'JAVASCRIPT'],
+
+      url: {
+        project: '#',
+        code: 'github.com/royeradames',
+      },
+      picture: {
+        sources: [
+          {
+            src: 'assets/images/thumbnail-project-5-small.webp',
+            media: '(max-width: 768px)',
+          },
+          {
+            src: 'assets/images/thumbnail-project-5-large.webp',
+            media: '(max-width: 1024px)',
+          },
+        ],
+        img: {
+          src: 'assets/images/thumbnail-project-5-small.webp',
+          alt: 'Project 1',
+        },
+      },
     },
     {
-      imgSmall: 'assets/images/thumbnail-project-6-small.webp',
-      imgLarge: 'assets/images/thumbnail-project-6-large.webp',
-      alt: 'Project 1',
       title: 'ART GALLERY SHOWCASE',
       skills: ['HTML', 'CSS', 'JAVASCRIPT'],
+
+      url: {
+        project: '#',
+        code: 'github.com/royeradames',
+      },
+      picture: {
+        sources: [
+          {
+            src: 'assets/images/thumbnail-project-6-small.webp',
+            media: '(max-width: 768px)',
+          },
+          {
+            src: 'assets/images/thumbnail-project-6-large.webp',
+            media: '(max-width: 1024px)',
+          },
+        ],
+        img: {
+          src: 'assets/images/thumbnail-project-6-small.webp',
+          alt: 'Project 1',
+        },
+      },
     },
   ];
+
 }
