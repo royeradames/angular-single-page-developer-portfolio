bf383c43fb03152da2e450cab2a8d2e612930701
Royer Adames <royeraadames@gmail.com>
2023-05-08 17:33:28 -0400

    update tag from app to svg-*

--- src/app/component/social-links/svg/frontend-mentor/frontend-mentor.component.ts
+++ src/app/component/social-links/svg/frontend-mentor/frontend-mentor.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-frontend-mentor',
+  selector: 'svg-frontend-mentor',
   templateUrl: './frontend-mentor.component.html',
   styleUrls: ['./frontend-mentor.component.scss']
 })
