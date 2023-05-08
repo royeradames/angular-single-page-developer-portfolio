65f2fe9bf82f64e471dc3c619622e5cd528ba673
Royer Adames <royeraadames@gmail.com>
2023-05-08 10:01:05 -0400

    use underline instead of a span

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
+      textUnderlineOffset: {
+        9: '17px',
+      },
       fontFamily: {
         "space-grotesk": ["Space Grotesk", "sans-serif"],
       },
