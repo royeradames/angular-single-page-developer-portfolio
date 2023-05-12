0c6704de7297de5621e78fc596d7e524375aa42b
Royer Adames <royeraadames@gmail.com>
2023-05-11 23:58:07 -0400

    simplify UserDataInterface

--- src/app/main/about/userData.ts
+++ src/app/main/about/userData.ts
@@ -1,38 +1,31 @@
-import { PictureInterface } from '../../model/picture.interface';
-
-export interface UserDataInterface {
-  name: string;
-  picture: PictureInterface;
-}
+import { UserDataInterface } from '../../model/user.interface';
 
 export const userData: UserDataInterface = {
-  picture: {
-    sources: [
-      {
-        src: 'assets/images/image-profile-mobile.webp',
-        media: '(min-width: 1px)',
-        height: '766',
-        width: '348',
-      },
-      {
-        src: 'assets/images/image-profile-tablet.webp',
-        media: '(min-width: 768px)',
-        height: '1200',
-        width: '646',
-      },
-      {
-        src: 'assets/images/image-profile-desktop.webp',
-        media: '(min-width: 1440px)',
-        height: '1440',
-        width: '890',
-      },
-    ],
-    img: {
+  sources: [
+    {
+      src: 'assets/images/image-profile-mobile.webp',
+      media: '(min-width: 1px)',
+      height: '766',
+      width: '348',
+    },
+    {
+      src: 'assets/images/image-profile-tablet.webp',
+      media: '(min-width: 768px)',
+      height: '1200',
+      width: '646',
+    },
+    {
       src: 'assets/images/image-profile-desktop.webp',
-      alt: 'Adam Keyes',
+      media: '(min-width: 1440px)',
       height: '1440',
       width: '890',
     },
+  ],
+  img: {
+    src: 'assets/images/image-profile-desktop.webp',
+    alt: 'Adam Keyes',
+    height: '1440',
+    width: '890',
   },
   name: 'Adam Keyes',
 };
---
+++ src/app/model/user.interface.ts
@@ -0,0 +1,8 @@
+import { SourceInterface } from './source.interface';
+import { ImgInterface } from './img.interface';
+
+export interface UserDataInterface {
+  name: string;
+  img: ImgInterface;
+  sources: SourceInterface[];
+}
