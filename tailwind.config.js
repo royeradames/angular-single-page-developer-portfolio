72776d30c3b72f993f35551cb1aeebb1057f95ef
Royer Adames <royeraadames@gmail.com>
2023-05-18 10:42:26 -0400

    rename colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,19 @@ module.exports = {
   theme: {
     extend: {
       textUnderlineOffset: {
-        9: '17px',
+        9: "17px",
       },
       fontFamily: {
         "space-grotesk": ["Space Grotesk", "sans-serif"],
       },
       colors: {
         custom: {
-          black: "#151515",
-          "dark-grey": "#242424",
-          green: "#4EE1A0",
-          grey: "#D9D9D9",
-          white: "#FFFFFF",
-          red: "#FF6F5B",
+          primary: "#D9D9D9",
+          secondary: "#242424",
+          background: "#151515",
+          accent: "#4EE1A0",
+          foreground: "#FFFFFF",
+          alert: "#FF6F5B",
         },
       },
     },
