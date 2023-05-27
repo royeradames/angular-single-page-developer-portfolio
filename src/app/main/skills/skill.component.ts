0d7abaff1e9ca950788b8acd02391ba87327e1da
Royer Adames <royeraadames@gmail.com>
2023-05-26 21:27:32 -0400

    hide or show the skills on mobile or smaller

--- src/app/main/skills/skill.component.ts
+++ src/app/main/skills/skill.component.ts
@@ -7,10 +7,15 @@ import { DataService } from '../../share/data.service';
   styleUrls: ['./skill.component.scss'],
 })
 export class SkillComponent {
+  isExpanded = false;
   skillPageData = this.dataService.skillPageData.value;
   constructor(private dataService: DataService) {
     this.dataService.skillPageData.subscribe((skillPageData) => {
       this.skillPageData = skillPageData;
     });
+  }
+
+  toggleIsExpanded(): void {
+    this.isExpanded = !this.isExpanded;
   }
 }
