562a97ab3288af681ccaff6f1f13695bb351da7e
Royer Adames <royeraadames@gmail.com>
2023-07-04 20:28:20 -0400

    refactor: simplify show and hide skills. avoid js to avoid slow down

--- src/app/main/skills/skill.component.ts
+++ src/app/main/skills/skill.component.ts
@@ -1,46 +1,33 @@
-import { Component, ElementRef, HostListener, OnDestroy } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { DataService } from '../../share/data.service';
-import { Subscription } from 'rxjs';
-import { GlobalRef } from '../../share/window-ref.injectable';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skill.component.html',
   styleUrls: ['./skill.component.scss'],
 })
-export class SkillComponent implements OnDestroy {
+export class SkillComponent implements AfterViewInit {
   isExpanded = false;
   skillPageData = this.dataService.skillPageData.value;
   spanButtonText = this.skillPageData.showButtonText;
-  trimmedSkillsCollection = this.skillPageData.Skills.slice(0, 10);
-  private mdQuerySize = 768;
-  skills = this.initSkills();
-  private dataSubscription?: Subscription;
+  skills = this.skillPageData.Skills;
+  @ViewChild('expandButton') expandButton!: ElementRef;
+  isButtonVisible: boolean | undefined;
+
   constructor(
     private dataService: DataService,
-    private elementRef: ElementRef,
-    private globalRef: GlobalRef
-  ) {
-    this.dataSubscription = this.dataService.skillPageData.subscribe(
-      (skillPageData) => {
-        this.skillPageData = skillPageData;
-      }
-    );
-    this.adjustSkills(this.globalRef.nativeWindow.innerWidth);
+    private elementRef: ElementRef
+  ) {}
+  ngAfterViewInit(): void {
+    this.isButtonVisible =
+      !this.expandButton.nativeElement.classList.contains('hidden');
   }
 
-  private initSkills() {
-    if (this.globalRef.nativeWindow.innerWidth <= this.mdQuerySize) {
-      return this.trimmedSkillsCollection;
-    }
-    return this.skillPageData.Skills;
-  }
   scrollToSkills(): void {
     const skillsSection =
       this.elementRef.nativeElement.querySelector('#skills');
-    if (skillsSection) {
-      skillsSection.scrollIntoView();
-    }
+    if (!skillsSection) return;
+    skillsSection.scrollIntoView();
   }
 
   toggleIsExpanded(): void {
@@ -48,26 +35,8 @@ export class SkillComponent implements OnDestroy {
     this.spanButtonText = this.isExpanded
       ? this.skillPageData.hideButtonText
       : this.skillPageData.showButtonText;
-    this.skills = this.isExpanded
-      ? this.skillPageData.Skills
-      : this.trimmedSkillsCollection;
     if (!this.isExpanded) {
       this.scrollToSkills();
     }
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(event: Event): void {
-    this.adjustSkills(this.globalRef.nativeWindow.innerWidth);
-  }
-
-  adjustSkills(windowWidth: number): void {
-    if (windowWidth > this.mdQuerySize) {
-      this.skills = this.skillPageData.Skills;
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.dataSubscription?.unsubscribe();
   }
 }
