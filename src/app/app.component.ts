808bb835e8a9c4c5401b9e0b4cb75cf52784d4f2
Royer Adames <royeraadames@gmail.com>
2023-07-05 14:28:13 -0400

    fix: page title in main tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { DataService } from './share/data.service';
-import { HeadMetaDataInterface } from './share/head-meta-data.facade';
 import { ThemeService } from './share/theme.service';
 
 @Component({
@@ -9,14 +7,7 @@ import { ThemeService } from './share/theme.service';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  headMetaData = this.dataService.headMetaData.value;
-  constructor(
-    private dataService: DataService,
-    private themeService: ThemeService
-  ) {
-    this.dataService.headMetaData.subscribe((headMetaData) => {
-      this.headMetaData = headMetaData;
-    });
+  constructor(private themeService: ThemeService) {
     //default theme is dark
     this.themeService.setDarkTheme(true);
   }
--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { DataService } from '../share/data.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {}
+export class MainComponent {
+  headMetaData = this.dataService.headMetaData.value;
+  constructor(private dataService: DataService) {}
+}
